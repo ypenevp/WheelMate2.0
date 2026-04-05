68c6a2f785ff1c7423917ec1836c020efdefaf0b
ypenevp <ypenevp@gmail.com>
2026-04-05 23:53:35 +0300

    setup: PlatformIO firmware

---
+++ hardware/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <MPU6050.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <TinyGPSPlus.h>
+#include "Adafruit_VL53L0X.h"
+#include "secrets.h"
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+
+void loop() {
+}
+
