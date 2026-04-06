d7031788aa715020667791dc0a1e2c5e5ee6ec3f
Bobi-77 <borislavstojnev8@gmail.com>
2026-04-06 13:14:34 +0300

    setup: react native expo frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = { 
+    content: [ "./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", 
+        "./components/**/*.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", ],
+     presets: [require("nativewind/preset")], 
+     theme: { extend: {}, }, plugins: [], };
