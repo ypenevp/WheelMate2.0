5581806a4fd761171f0a6941f2c3ced329ea6de0
ypenevp <ypenevp@gmail.com>
2026-04-09 00:55:29 +0300

    feat(hardware): setup MQTT connection with backend

--- backend/src/main/java/com/legendss/backend/configs/MqqtConfig.java
+++ backend/src/main/java/com/legendss/backend/configs/MqqtConfig.java
@@ -1,4 +1,34 @@
 package com.legendss.backend.configs;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MqqtConfig {
+
+    @Value("${mqtt.broker.url}")
+    private String brokerUrl;
+
+    @Value("${mqtt.client.id}")
+    private String clientId;
+
+    @Bean
+    public MqttClient mqttClient() throws MqttException {
+        MqttClient client = new MqttClient(brokerUrl, clientId, new MemoryPersistence());
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+
+        System.out.println("Connecting to MQTT Broker: " + brokerUrl);
+        client.connect(options);
+
+        return client;
+    }
 }
--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 #include <MPU6050.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -8,12 +11,90 @@
 #include "Adafruit_VL53L0X.h"
 #include "secrets.h"
 
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
-void setup() {
-  Serial.begin(115200);
+unsigned long lastMsgTime = 0;
+const unsigned long MSG_INTERVAL = 5000;
+
+void setupWiFi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to WiFi: ");
+    Serial.println(WIFI_SSID);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
+void reconnectMqtt() {
+    while (!mqttClient.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if (mqttClient.connect(MQTT_CLIENT_ID)) {
+            Serial.println("connected to MQTT broker!");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(mqttClient.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+void publishTelemetry() {
+    String fakeLocation = "42.6973045, 23.32176172";
+    bool fakeUserInChair = true;
+    bool fakePanic = false;
+    bool dummyFakePanic = false;
+
+    JsonDocument doc;
+    doc["location"] = fakeLocation;
+    doc["userInChair"] = fakeUserInChair;
+    doc["panic"] = fakePanic;
+    doc["fakePanic"] = dummyFakePanic;
+
+    String jsonString;
+    serializeJson(doc, jsonString);
+
+    String topic = String("wheelmate/telemetry/") + String(WHEELCHAIR_DB_ID);
+
+    if(mqttClient.publish(topic.c_str(), jsonString.c_str())) {
+        Serial.println("Published to " + topic + ": " + jsonString);
+    } else {
+        Serial.println("Failed to publish telemetry :(");
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    setupWiFi();
+    mqttClient.setServer(MQTT_BROKER_IP, MQTT_PORT);
+}
 
 void loop() {
+    if (WiFi.status() != WL_CONNECTED) {
+        setupWiFi();
+    }
+
+    if (!mqttClient.connected()) {
+        reconnectMqtt();
+    }
+    
+    mqttClient.loop();
+
+    unsigned long now = millis();
+    if (now - lastMsgTime > MSG_INTERVAL) {
+        lastMsgTime = now;
+        publishTelemetry();
+    }
 }
 
