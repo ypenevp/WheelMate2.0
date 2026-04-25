22cb99a658c885fc5bdf17afc8a4ea14b4ce1f7e
DeyanAzSum <deyan662@gmail.com>
2026-04-25 12:18:50 +0300

    feat(backend): Add mobility functionality

---
+++ backend/src/main/java/com/legendss/backend/entities/Mobility.java
@@ -0,0 +1,38 @@
+package com.legendss.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mobility")
+@Getter
+@Setter
+public class Mobility {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "mobility")
+    private Boolean imobility;
+
+    @Column(name = "starttime")
+    private LocalDateTime starttime;
+
+    @Column(name = "finishtime")
+    private LocalDateTime finishtime;
+
+    @ManyToOne
+    @JoinColumn(name = "wheelchair_id", nullable = false)
+    private Wheelchair wheelchair;
+
+    @PrePersist
+    protected void onCreate() {
+        this.finishtime = LocalDateTime.now();
+        this.starttime = this.finishtime.minusSeconds(10);
+    }
+
+}
